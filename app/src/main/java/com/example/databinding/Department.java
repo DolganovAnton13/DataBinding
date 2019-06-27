7cf8d725d09a0ba9c4c9d8fd25cc262d038bcf2e
Antoha1397 <toscha.17@mail.ru>
2019-06-27 14:08:27 +0500

    Add department

---
+++ app/src/main/java/com/example/databinding/Department.java
@@ -0,0 +1,13 @@
+package com.example.databinding;
+
+public class Department {
+
+    public Department(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long id;
+
+    public String name;
+}
--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -12,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Employee employee = new Employee(1, "John Smith", "London", 10000);
+        Department department = new Department(100, "IT");
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setEmployee(employee);
+        binding.setDepartment(department);
     }
 }
